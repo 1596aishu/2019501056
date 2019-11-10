9068bfb055afb9893fb8d549bb5e17e4e3018bd9
Aiswarya <1596aishu@gmail.com>
2019-11-11 01:02:52 +0530

    Day9-Assignment2

--- ADS1/Day9/Assignment2/LinearProbingHashST.java
+++ ADS1/Day9/Assignment2/LinearProbingHashST.java
@@ -1,70 +1,27 @@
-// @ author -- Harika
-public class LinearProbingHashST<Key, Value> {
-    private static final int INIT_CAPACITY = 4;
-
-    private int n;           // number of key-value pairs in the symbol table
-    private int m;           // size of linear probing table
-    private Key[] keys;      // the keys
-    private Value[] vals;    // the values
-
-
-    /**
-     * Initializes an empty symbol table.
-     */
-    public LinearProbingHashST() {
-        this(INIT_CAPACITY);
+public class LinearProbingHashST<Key, Value>{
+    private static final int CAPACITY = 4;
+    public int n;           
+    public int m;           
+    public Key[] keys;      
+    public Value[] vals; 
+    public LinearProbingHashST(){
+        this(CAPACITY);
     }
-
-    /**
-     * Initializes an empty symbol table with the specified initial capacity.
-     *
-     * @param capacity the initial capacity
-     */
-    public LinearProbingHashST(int capacity) {
+    public LinearProbingHashST(int capacity){
         m = capacity;
         n = 0;
-        keys = (Key[])   new Object[m];
+        keys = (Key[]) new Object[m];
         vals = (Value[]) new Object[m];
     }
-
-    /**
-     * Returns the number of key-value pairs in this symbol table.
-     *
-     * @return the number of key-value pairs in this symbol table
-     */
-    public int size() {
+    public int size(){
         return n;
     }
-
-    /**
-     * Returns true if this symbol table is empty.
-     *
-     * @return {@code true} if this symbol table is empty;
-     *         {@code false} otherwise
-     */
-    public boolean isEmpty() {
+    public boolean isEmpty(){
         return size() == 0;
     }
-
-    /**
-     * Returns true if this symbol table contains the specified key.
-     *
-     * @param  key the key
-     * @return {@code true} if this symbol table contains {@code key};
-     *         {@code false} otherwise
-     * @throws IllegalArgumentException if {@code key} is {@code null}
-     */
     public boolean contains(Key key) {
-        if (key == null) throw new IllegalArgumentException("argument to contains() is null");
         return get(key) != null;
     }
-
-    // hash function for keys - returns value between 0 and M-1
-    private int hash(Key key) {
-        return (key.hashCode() & 0x7fffffff) % m;
-    }
-
-    // resizes the hash table to the given capacity by re-hashing all of the keys
     private void resize(int capacity) {
         LinearProbingHashST<Key, Value> temp = new LinearProbingHashST<Key, Value>(capacity);
         for (int i = 0; i < m; i++) {
@@ -74,30 +31,17 @@ public class LinearProbingHashST<Key, Value> {
         }
         keys = temp.keys;
         vals = temp.vals;
-        m    = temp.m;
+        m = temp.m;
     }
-
-    /**
-     * Inserts the specified key-value pair into the symbol table, overwriting the old 
-     * value with the new value if the symbol table already contains the specified key.
-     * Deletes the specified key (and its associated value) from this symbol table
-     * if the specified value is {@code null}.
-     *
-     * @param  key the key
-     * @param  val the value
-     * @throws IllegalArgumentException if {@code key} is {@code null}
-     */
-    public void put(Key key, Value val) {
-        if (key == null) throw new IllegalArgumentException("first argument to put() is null");
-
-        if (val == null) {
+    private int hash(Key key){
+        return (key.hashCode()) % m;
+    }
+    public void put(Key key, Value val){
+        if (val == null){
             delete(key);
             return;
         }
-
-        // double table size if 50% full
         if (n >= m/2) resize(2*m);
-
         int i;
         for (i = hash(key); keys[i] != null; i = (i + 1) % m) {
             if (keys[i].equals(key)) {
@@ -109,92 +53,49 @@ public class LinearProbingHashST<Key, Value> {
         vals[i] = val;
         n++;
     }
-
-    /**
-     * Returns the value associated with the specified key.
-     * @param key the key
-     * @return the value associated with {@code key};
-     *         {@code null} if no such value
-     * @throws IllegalArgumentException if {@code key} is {@code null}
-     */
-    public Value get(Key key) {
+    public Value get(Key key){
         if (key == null) throw new IllegalArgumentException("argument to get() is null");
         for (int i = hash(key); keys[i] != null; i = (i + 1) % m)
             if (keys[i].equals(key))
                 return vals[i];
         return null;
     }
-
-    /**
-     * Removes the specified key and its associated value from this symbol table     
-     * (if the key is in this symbol table).    
-     *
-     * @param  key the key
-     * @throws IllegalArgumentException if {@code key} is {@code null}
-     */
-    public void delete(Key key) {
+    public Iterable<Key> keys(){
+        Queue<Key> Q = new Queue<Key>();
+        for (int i = 0; i < m; i++)
+            if (keys[i] != null) Q.enqueue(keys[i]);
+        return Q;
+    }
+    public void delete(Key key){
         if (key == null) throw new IllegalArgumentException("argument to delete() is null");
         if (!contains(key)) return;
-
-        // find position i of key
         int i = hash(key);
         while (!key.equals(keys[i])) {
             i = (i + 1) % m;
         }
-
-        // delete key and associated value
         keys[i] = null;
         vals[i] = null;
-
-        // rehash all keys in same cluster
         i = (i + 1) % m;
-        while (keys[i] != null) {
-            // delete keys[i] an vals[i] and reinsert
-            Key   keyToRehash = keys[i];
-            Value valToRehash = vals[i];
+        while (keys[i] != null){
+            Key   Rkey = keys[i];
+            Value Rval = vals[i];
             keys[i] = null;
             vals[i] = null;
             n--;
-            put(keyToRehash, valToRehash);
+            put(Rkey, Rval);
             i = (i + 1) % m;
         }
-
         n--;
-
-        // halves size of array if it's 12.5% full or less
         if (n > 0 && n <= m/8) resize(m/2);
-
-        assert check();
-    }
-
-    /**
-     * Returns all keys in this symbol table as an {@code Iterable}.
-     * To iterate over all of the keys in the symbol table named {@code st},
-     * use the foreach notation: {@code for (Key key : st.keys())}.
-     *
-     * @return all keys in this symbol table
-     */
-    public Iterable<Key> keys() {
-        Queue<Key> queue = new Queue<Key>();
-        for (int i = 0; i < m; i++)
-            if (keys[i] != null) queue.enqueue(keys[i]);
-        return queue;
-    }
-
-    // integrity check - don't check after each put() because
-    // integrity not maintained during a delete()
+        check();
+    }    
     private boolean check() {
-
-        // check that hash table is at most 50% full
         if (m < 2*n) {
             System.err.println("Hash table size m = " + m + "; array size n = " + n);
             return false;
         }
-
-        // check that each key in table can be found by get()
         for (int i = 0; i < m; i++) {
-            if (keys[i] == null) continue;
-            else if (get(keys[i]) != vals[i]) {
+            if (get(keys[i]) != vals[i]) {
                 System.err.println("get[" + keys[i] + "] = " + get(keys[i]) + "; vals[i] = " + vals[i]);
                 return false;
             }
@@ -202,9 +103,10 @@ public class LinearProbingHashST<Key, Value> {
         return true;
     }
     public static void main(String[] args) {
-        LinearProbingHashST l = new LinearProbingHashST();
-        l.put("A", 1);
-        l.put("B", 4);
-        System.out.println(l.get("A"));
+        LinearProbingHashST hash = new LinearProbingHashST();
+        hash.put("xyz", 12);
+        hash.put("abc", 15);
+        System.out.println(hash.get("cde"));
+        System.out.println(hash.get("abc"));
     }
 }
