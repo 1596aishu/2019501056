f65ccb74e8624ada7c12e6c22bb6f97cd5444cd7
1596aishu <55698607+1596aishu@users.noreply.github.com>
2020-01-06 19:58:01 +0530

    Add files via upload

---
+++ ADS2/Day1/Assignment1/Assignment1.java
@@ -0,0 +1,55 @@
+import java.io.*;  
+
+class Assignment1{
+    public static void main(String args[]) throws Exception{ 
+        Assignment1 as = new Assignment1();
+        String synsets = "C:\\Users\\aishwarya\\Desktop\\2019501056\\ADS2\\Day1\\Assignment1\\wordNet\\synsets.txt";
+        String hypernyms = "C:\\Users\\aishwarya\\Desktop\\2019501056\\ADS2\\Day1\\Assignment1\\wordNet\\hypernyms.txt";
+        as.synsets_file(synsets);
+        as.hypernyms_file(hypernyms);
+    }
+
+    private void synsets_file(String file) throws Exception{        
+            FileReader fr = new FileReader(file);    
+            BufferedReader br = new BufferedReader(fr);
+            String j;
+            String[] str = new String[100];
+            String[] key = new String[100];
+            String[] val = new String[100];
+            int c = 0;
+            int y = 0;
+            while((j = br.readLine())!=null){
+                int i = 0;
+                if(c<5){
+                    System.out.println(j);
+                    str = j.split(",",2);
+                    key[y]=str[i];
+                    val[y]=str[i+1];
+                }
+                i++;
+                y++;
+                c = c+1;
+            }
+            for (int a =0; a<5;a++) 
+                System.out.println(key[a]+ " : {"+ val[a]+"}");
+            fr.close();    
+        } 
+        
+    private void hypernyms_file(String file) throws Exception{
+        FileReader fr = new FileReader(file);    
+        BufferedReader br = new BufferedReader(fr);
+        String j;
+        int c = 0;
+        String[] str = new String[100];
+        while((j = br.readLine())!=null){
+            int i = 0;
+            if(c<5){
+                System.out.println(j);
+                str = j.split(",",2);
+            } c=c+1;
+        }
+        for (int a = 0; a<str.length;a++) 
+            System.out.print(str[a]);
+        fr.close();    
+    }
+}
