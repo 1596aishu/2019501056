ec1d3948afebf198b2e3ae4e649913c880a0957c
1596aishu <55698607+1596aishu@users.noreply.github.com>
2020-03-13 16:37:17 +0530

    Add files via upload

--- CN/Day2/Assignment-4/udp_client.py
+++ CN/Day2/Assignment-4/udp_client.py
@@ -2,7 +2,6 @@ import socket
 host = '127.0.0.1'
 port = 65432
 with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
-	s.connect((host,port))
-	s.sendall(b'hello aishu')
-	data = s.recv(1024)
+	s.sendall(b'Hello aishu', (host,port))
+	data = s.recvrom(1024)
 print('recieved', repr(data))
--- CN/Day2/Assignment-4/udp_server.py
+++ CN/Day2/Assignment-4/udp_server.py
@@ -3,12 +3,10 @@ host = '127.0.0.1'
 port = 65432
 with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
 	s.bind((host,port))
-	s.listen()
-	conn, addr = s.accept()
-	with conn:
-		print('Connected by ', addr)
-		while True:
-			data = conn.recv(1024)
-			if not data:
-				break
-			conn.sendall(data)
+	while True:
+		data = conn.recv(1024)
+		if not data:
+			break
+		print(data[0])
+        s.sendto(b"Hello", data[1])
+		
